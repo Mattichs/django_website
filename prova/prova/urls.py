97236a668f210ec1b5601047eddad1cddc5b7a0d
Mattichs <mattiabastia@gmail.com>
2023-05-10 16:15:17 +0200

    added login route

--- prova/prova/urls.py
+++ prova/prova/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('photos.urls')),
+    path('login/', include('django.contrib.auth.urls')),
+    path('login/', include('members.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
